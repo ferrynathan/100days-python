1ab4dc8b1c533c3123d7646ffeb870af086a911d
Nathan FERRY <nathan.ferry@accenture.com>
2022-11-11 18:41:04 +0100

    day1

--- day1/main.py
+++ day1/main.py
@@ -1,3 +1,7 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+print("Hello World\nHello World\nHello World")
+print("Hello" + " Nathan")
+print("Hello" + " " + "Nathan")
