7cc948a0fee3011a1fa109dfe27548a870274679
Nathan FERRY <nathan.ferry@accenture.com>
2022-11-22 18:02:53 +0100

    day4

--- day4/basic-lists.py
+++ day4/basic-lists.py
@@ -2,3 +2,6 @@ america_states = ["delaware", "new jersey", "masachussets", "new york"]
 print("the first state is " + america_states[0])
 print("the third state is " + america_states[2])
 print("the last state is " + america_states[-1])
+
+america_states[2] = "prout"
+print(america_states)
