7d451a69faf23aba5a182d22aefd865d75d0ff52
Nathan FERRY <nathan.ferry@accenture.com>
2022-12-24 10:26:01 +0100

    day5

--- day5/range-even.py
+++ day5/range-even.py
@@ -3,3 +3,9 @@ for number in range(2, 101, 2):
     print(number)
     total += number
 print(total)
+
+total2 = 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        total2 += number
+print(total2)
