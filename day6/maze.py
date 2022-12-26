8f78ee2cf393051a8d1df84d1fd498c8d764e921
Nathan FERRY <nathan.ferry@accenture.com>
2022-12-26 12:01:51 +0100

    day6

---
+++ day6/maze.py
@@ -0,0 +1,15 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if wall_on_right():
+        if wall_in_front():
+            turn_left()
+        else:
+            move()
+    else:
+        turn_right()
+        move()
