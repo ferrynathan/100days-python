404f2042888a317b40804ddadd154c518db72fd4
Nathan FERRY <nathan.ferry@accenture.com>
2022-12-26 09:35:30 +0100

    day6

---
+++ day6/hurdles-while.py
@@ -0,0 +1,21 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+number_of_hurdles = 6
+while number_of_hurdles > 0:
+    number_of_hurdles -= 1
+    jump()
