fd52c64c8537951af4e13efb724325cf15667cd1
Nathan FERRY <nathan.ferry@accenture.com>
2022-12-28 10:02:55 +0100

    day6

--- day6/maze.py
+++ day6/maze.py
@@ -4,12 +4,17 @@ def turn_right():
     turn_left()
 
 
+detect_loop = 0
 while not at_goal():
     if wall_on_right():
+        detect_loop = 0
         if wall_in_front():
             turn_left()
         else:
             move()
     else:
+        detect_loop += 1
+        if detect_loop == 5:
+            turn_left()
         turn_right()
         move()
