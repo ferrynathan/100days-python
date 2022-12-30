405145d99da005e31271b63998d8cd7982e5929b
Nathan FERRY <nathan.ferry@accenture.com>
2022-12-30 22:21:39 +0100

    day7

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,10 +1,74 @@
-# Step 3
+# Step 4
 
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+stages = [
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+'''
+]
+
+end_of_game = False
+user_score = 7
+word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+# Set 'lives' to equal 6.
 
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -14,19 +78,34 @@ display = []
 for _ in range(word_length):
     display += "_"
 
-print(display)
-
-# TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-
-# Check guessed letter
-blanks = True
-while blanks:
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
+    correct_guess = 0
+    # Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
-    print(display)
-    if '_' not in display:
-        blanks = False
-        print('You win!')
+            correct_guess += 1
+    if correct_guess == 0:
+        user_score -= 1
+        print(f"User score = {user_score}")
+        print(stages[user_score])
+
+    # TODO-2: - If guess is not a letter in the chosen_word,
+    # Then reduce 'lives' by 1.
+    # If lives goes down to 0 then the game should stop and it should print "You lose."
+
+    # Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    # Check if user has got all letters.
+    if user_score == 0:
+        end_of_game = True
+        print("You lose.")
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
