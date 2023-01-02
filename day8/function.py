41108a2c290eefd56978d1985110849e76228779
Nathan FERRY <nathan.ferry@accenture.com>
2023-01-02 16:00:26 +0100

    day8

---
+++ day8/function.py
@@ -0,0 +1,19 @@
+def greet():
+    print("Hello !")
+    print("Welcome to my home !")
+    print("I'm Drakuul")
+
+
+greet()
+
+# name est le paramètre
+# Nathan est l'argument
+
+
+def greet_with_input(name):
+    print(f"Hello {name}!")
+    print("Welcome to my home !")
+    print("I'm Drakuul")
+
+
+greet_with_input("Nathan")
